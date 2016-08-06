d1fa6d2e1b70c58e82a4fdafd87fa176a5f3d996
hegdeashwin <ashwin.hegde3@gmail.com>
2016-08-06 23:54:15 +0530

    Adds Type eg1: Boolean & number

---
+++ codes/Ch2_Types/Eg1/index.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let isDone: boolean = false;
+let decimal: number = 45;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+console.log('Boolean: ', isDone);
+console.log('Decimal: ', decimal);
+console.log('Hex: ', hex);
+console.log('Binary: ', binary);
+console.log('Octal: ', octal);
